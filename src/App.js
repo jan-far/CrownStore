5a4b318fef230dc9ca8b8b1adedeb022d93248dc
jan-far <ligalifarouk@gmail.com>
2020-12-25 09:06:46 +0100

    Update MenuItem to navigate using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import HomePage from './pages/Homepage';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Switch>
+          <Route exact path='/shop/hats' component={}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
