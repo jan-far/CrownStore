cf4f04488b8ab31bb8888440a4621a3d9b30879c
jan-far <ligalifarouk@gmail.com>
2021-01-01 20:08:49 +0100

    Added Stripe for Payment
    Integrated the Stripe API in test mode,to handle payments using the react library, react-stripe-checkout.
    Change the favicon and title for the page

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckOutItem from '../../Components/CheckOutItem';
+import StripeCheckoutButton from '../../Components/Stripe';
 import {
   selectCartItem,
   selectCartTotal,
@@ -33,6 +34,12 @@ const CheckOutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total}/>
     </div>
   );
 };
