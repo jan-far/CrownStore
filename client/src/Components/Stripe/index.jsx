82b2a69b108d95c969013811aa3468f17a6d0455
jan-far <ligalifarouk@gmail.com>
2021-03-01 11:22:37 +0100

    Feature: Added express server and separated app files
    Separated app file into client folder
    Created express server file to handle backend stripe payments

--- client/src/Components/Stripe/index.jsx
+++ client/src/Components/Stripe/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51I4rxfLYKKj1BXXIpnA8HUG8CFUovug5jNZeYUvZ4cWiqCsS3QHiiAqRpaSx6bUekkAPpe0omt1429ng3hW4y99h00yNrIaxVf';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((err) => {
+        console.log('Payment error', JSON.parse(err));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card'
+        );
+      });
   };
 
   return (
